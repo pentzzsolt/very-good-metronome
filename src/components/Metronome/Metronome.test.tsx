49cf564886c69f3fd2a187d6247c421b139ad0c0
Zsolt Pentz <3056515+pentzzsolt@users.noreply.github.com>
2023-08-09 22:19:01 +0200

    Skip Metronome tests

--- src/components/Metronome/Metronome.test.tsx
+++ src/components/Metronome/Metronome.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Metronome } from './Metronome';
 
-test('renders learn react link', () => {
+test.skip('renders learn react link', () => {
   render(<Metronome />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
