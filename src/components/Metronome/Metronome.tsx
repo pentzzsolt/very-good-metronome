67feaa2820fa7e9b3dfe61b038d5ae3c7a05b088
Zsolt Pentz <3056515+pentzzsolt@users.noreply.github.com>
2023-08-12 11:41:33 +0200

    Set basic layout

--- src/components/Metronome/Metronome.tsx
+++ src/components/Metronome/Metronome.tsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import { Button, Footer } from './components';
 
 export const Metronome = (): JSX.Element => {
-  const [ count, setCount ] = useState(1);
-  const [ interval, setInterval ] = useState<number | undefined>();
-  const [ isPlaying, setIsPlaying ] = useState(false);
-  const [ tempo, setTempo ] = useState(60);
-  const [ timeSignature, setTimeSignature ] = useState('4/4');
+  const [count, setCount] = useState(1);
+  const [interval, setInterval] = useState<number | undefined>();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [tempo, setTempo] = useState(60);
+  const [timeSignature, setTimeSignature] = useState('4/4');
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setTempo(parseInt(event.target.value))
@@ -34,15 +34,21 @@ export const Metronome = (): JSX.Element => {
   return (
     <>
       <h1>{count}</h1>
-      <label htmlFor="timeSignature">Time signature</label>
-      <select onChange={handleTimeSignatureChange} value={timeSignature} id="timeSignature">
-        <option value="2/4">2/4</option>
-        <option value="3/4">3/4</option>
-        <option value="4/4">4/4</option>
-      </select>
-      <Button onPress={handlePress}>{isPlaying ? "Stop" : "Start"}</Button>
-      <label htmlFor="tempo">Tempo: {tempo}</label>
-      <input id="tempo" type="range" min="30" max="200" onChange={handleChange} value={tempo} step="1"></input>
+      <div>
+        <Button onPress={handlePress}>{isPlaying ? "Stop" : "Start"}</Button>
+        <div>
+          <label htmlFor="timeSignature">Time signature</label>
+          <select onChange={handleTimeSignatureChange} value={timeSignature} id="timeSignature">
+            <option value="2/4">2/4</option>
+            <option value="3/4">3/4</option>
+            <option value="4/4">4/4</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="tempo">Tempo: {tempo}</label>
+          <input id="tempo" type="range" min="30" max="200" onChange={handleChange} value={tempo} step="1"></input>
+        </div>
+      </div>
       <Footer />
     </>
   )
