6969e209bf4ab12e2e2af4d0c26ce87eca1b9625
Zsolt Pentz <3056515+pentzzsolt@users.noreply.github.com>
2023-08-13 10:30:43 +0200

    Increase font size of Count

--- src/components/Metronome/components/Counter/Counter.tsx
+++ src/components/Metronome/components/Counter/Counter.tsx
@@ -1,3 +1,7 @@
 import { CounterProps } from "./types/CounterProps";
 
-export const Counter = ({ children }: CounterProps): JSX.Element => <h1>{children}</h1>;
+export const Counter = ({ children }: CounterProps): JSX.Element => <h1 style={{
+  fontSize: "30vh",
+  margin: 0,
+  paddingTop: ".25em"
+}}>{children}</h1>;
