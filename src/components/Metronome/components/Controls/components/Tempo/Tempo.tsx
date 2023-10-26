adfcf7097325b92fc1e68f6752558016660c387d
Zsolt Pentz <3056515+pentzzsolt@users.noreply.github.com>
2023-10-26 16:48:13 +0200

    Change code to use generated unique ids

--- src/components/Metronome/components/Controls/components/Tempo/Tempo.tsx
+++ src/components/Metronome/components/Controls/components/Tempo/Tempo.tsx
@@ -1,18 +1,23 @@
+import { useId } from "react";
 import { TempoProps } from "./types/TempoProps";
 
-export const Tempo = ({ onChange, value }: TempoProps): JSX.Element => (
-  <>
-    <div style={{
-      display: "flex",
-      justifyContent: "space-between"
-    }}>
-      <label htmlFor="tempo">Tempo:</label>
-      <output>{value}</output>
-    </div>
-    <div>
-      <input id="tempo" style={{
-        width: "100%"
-      }} type="range" min="30" max="200" step="1" onChange={onChange} value={value}></input>
-    </div>
-  </>
-);
+export const Tempo = ({ onChange, value }: TempoProps): JSX.Element => {
+  const id = useId();
+
+  return (
+    <>
+      <div style={{
+        display: "flex",
+        justifyContent: "space-between"
+      }}>
+        <label htmlFor={id}>Tempo:</label>
+        <output>{value}</output>
+      </div>
+      <div>
+        <input id={id} style={{
+          width: "100%"
+        }} type="range" min="30" max="200" step="1" onChange={onChange} value={value}></input>
+      </div>
+    </>
+  )
+};
--- src/components/Metronome/components/Controls/components/TimeSignature/TimeSignature.tsx
+++ src/components/Metronome/components/Controls/components/TimeSignature/TimeSignature.tsx
@@ -1,12 +1,17 @@
+import { useId } from "react";
 import { TimeSignatureProps } from "./types/TimeSignatureProps";
 
-export const TimeSignature = ({ onChange, value }: TimeSignatureProps) => (
-  <>
-    <label htmlFor="timeSignature">Time signature</label>
-    <select id="timeSignature" onChange={onChange} value={value}>
-      <option value="2/4">2/4</option>
-      <option value="3/4">3/4</option>
-      <option value="4/4">4/4</option>
-    </select>
-  </>
-);
+export const TimeSignature = ({ onChange, value }: TimeSignatureProps) => {
+  const id = useId();
+
+  return (
+    <>
+      <label htmlFor={id}>Time signature</label>
+      <select id={id} onChange={onChange} value={value}>
+        <option value="2/4">2/4</option>
+        <option value="3/4">3/4</option>
+        <option value="4/4">4/4</option>
+      </select>
+    </>
+  )
+};
